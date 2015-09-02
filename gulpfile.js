23b97daa68fb7061142528936fd014f427d2f4ae
Matthew McKinley <matt@silverlinecrm.com>
2015-09-02 14:31:55 -0400

    updated gulpfile to zip static resource

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')({
     lazy: false
 });
+var gutil = require('gulp-util');
 var order = require('gulp-order');
 var mainBowerFiles = require('main-bower-files');
 var gulpFilter = require('gulp-filter');
@@ -57,7 +58,43 @@ gulp.task('dependencies', function() {
         .pipe(gulp.dest('./build'));
 });
 //for making static resource
+function createFileFromString(filename, string) {
+    var src = require('stream').Readable({
+        objectMode: true
+    })
+    src._read = function() {
+        this.push(new gutil.File({
+            cwd: "",
+            base: "",
+            path: filename,
+            contents: new Buffer(string)
+        }))
+        this.push(null)
+    }
+    return src
+}
+gulp.task('zip-staticresource', function() {
+    return gulp.src('build/**/*')
 
+    .pipe(plugins.zip('ngForce1.resource'))
+        .pipe(gulp.dest('../src/staticresources'));
+});
+
+gulp.task('meta-staticresource', function() {
+    return createFileFromString('ngForce1.resource-meta.xml', '<?xml version="1.0" encoding="UTF-8"?><StaticResource xmlns="http://soap.sforce.com/2006/04/metadata"><cacheControl>Private</cacheControl><contentType>application/octet-stream</contentType></StaticResource>')
+        .pipe(gulp.dest('../src/staticresources'));
+});
+
+gulp.task('meta-page', function() {
+    return createFileFromString('ngForce1.page-meta.xml', '<?xml version="1.0" encoding="UTF-8"?><ApexPage xmlns="http://soap.sforce.com/2006/04/metadata"><apiVersion>33.0</apiVersion><availableInTouch>true</availableInTouch><label>ngForce1</label></ApexPage>')
+        .pipe(gulp.dest('../src/pages'));
+});
+
+gulp.task('vf-page', function() {
+    return gulp.src('./app/ngForce1.page')
+        .pipe(gulp.dest('../src/pages'));
+});
+gulp.task('save-static-resource-zip', ['meta-staticresource', 'zip-staticresource']);
 //for making minified src
 gulp.task('combine', ['ngForceWithDependencies', 'ngForceWithDependenciesMinified']);
 gulp.task('build', ['dependencies', 'ngForce']);
